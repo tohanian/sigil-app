cb2bdcba0fbbe8a4006a4de84e112fab24201585
github username <lina.rudashevski@gmail.com>
2019-01-20 17:49:22 -0500

    finished updating fonts

--- src/icons/fonts.js
+++ src/icons/fonts.js
@@ -2,77 +2,97 @@ import * as React from "react";
 import css from "../App.css";
 
 export const Amatic_SC = ({ fontFam = "Amatic_SC" }) => (
-  <div className={fontFam}> Amatic </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
 export const Bungee_Shade = ({ fontFam = "Bungee_Shade" }) => (
-  <div className={fontFam}> Bungee </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
 export const Charmonman = ({ fontFam = "Charmonman" }) => (
-  <div className={fontFam}> {fontFam} </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
+);
+
+export const Creepster = ({ fontFam = "Creepster" }) => (
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
 export const Special_Elite = ({ fontFam = "Special_Elite" }) => (
-  <div className={fontFam}> Elite </div>
-);
-
-export const Gamja_Flower = ({ fontFam = "Gamja_Flower" }) => (
-  <div className={fontFam}> Gamja</div>
-);
-
-export const Gloria_Hallelujah = ({ fontFam = "Gloria_Hallelujah" }) => (
-  <div className={fontFam}> Gloria </div>
-);
-
-export const Indie_Flower = ({ fontFam = "Indie_Flower" }) => (
-  <div className={fontFam}> Indie</div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
 export const Faster_One = ({ fontFam = "Faster_One" }) => (
-  <div className={fontFam}> Faster </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
 export const Fredericka_the_Great = ({ fontFam = "Fredericka_the_Great" }) => (
-  <div className={fontFam}> Fred </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
+);
+
+export const Gaegu = ({ fontFam = "Gaegu" }) => (
+  <div className={fontFam}> House Stark; Our way is the old way </div>
+);
+
+export const Gamja_Flower = ({ fontFam = "Gamja_Flower" }) => (
+  <div className={fontFam}> House Stark; Our way is the old way</div>
+);
+
+export const Crafty_Girls = ({ fontFam = "Crafty_Girls" }) => (
+  <div className={fontFam}> House Stark; Our way is the old way </div>
+);
+
+export const Gloria_Hallelujah = ({ fontFam = "Gloria_Hallelujah" }) => (
+  <div className={fontFam}> House Stark; Our way is the old way </div>
+);
+
+export const Indie_Flower = ({ fontFam = "Indie_Flower" }) => (
+  <div className={fontFam}> House Stark; Our way is the old way</div>
 );
 
 export const Lato = ({ fontFam = "Lato" }) => (
-  <div className={fontFam}> {fontFam} </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
 export const Lobster = ({ fontFam = "Lobster" }) => (
-  <div className={fontFam}> {fontFam} </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
+);
+
+export const Permanent_Marker = ({ fontFam = "Permanent_Marker" }) => (
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
 export const Mountains_of_Christmas = ({
   fontFam = "Mountains_of_Christmas"
-}) => <div className={fontFam}> Mountains </div>;
+}) => <div className={fontFam}> House Stark; Our way is the old way </div>;
 
 export const Oswald = ({ fontFam = "Oswald" }) => (
-  <div className={fontFam}> {fontFam} </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
-export const Permanent_Marker = ({ fontFam = "Permanent_Marker" }) => (
-  <div className={fontFam}> Marker </div>
+export const Poiret_One = ({ fontFam = "Poiret_One" }) => (
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
 export const Poppins = ({ fontFam = "Poppins" }) => (
-  <div className={fontFam}> {fontFam} </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
 export const Raleway = ({ fontFam = "Raleway" }) => (
-  <div className={fontFam}> {fontFam} </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
 export const Rock_Salt = ({ fontFam = "Rock_Salt" }) => (
-  <div className={fontFam}> Rock Salt </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
+);
+
+export const Stalemate = ({ fontFam = "Stalemate" }) => (
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
 export const Berkshire_Swash = ({ fontFam = "Berkshire_Swash" }) => (
-  <div className={fontFam}> Swash </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
 
 export const Unlock = ({ fontFam = "Unlock" }) => (
-  <div className={fontFam}> {fontFam} </div>
+  <div className={fontFam}> House Stark; Our way is the old way </div>
 );
