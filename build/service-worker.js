ae5f8e48d5c92d926931e0a1f3daf9b37dbf1dee
github username <lina.rudashevski@gmail.com>
2019-02-07 19:06:07 -0500

    fix font bug

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d1c72b33efc96f8f1337f79ac1c5f70.js"
+  "/precache-manifest.bd0d957363eadef07b96dced7df20270.js"
 );
 
 workbox.clientsClaim();
